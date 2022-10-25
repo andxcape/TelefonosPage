4690d89746875e7e1a1b64f3cc72f45570af1f3d
gperez2018345 <gperez-2018345@kinal.edu.gt>
2022-10-25 16:59:17 -0600

    ch

--- telefonosBack/index.js
+++ telefonosBack/index.js
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/telefonos', { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
     console.log("Se encuentra conectado a la base de datos.");
 
-    app.listen(3000, function () {
+    app.listen(8080, function () {
     })
 
 }).catch(error => console.log(error));
